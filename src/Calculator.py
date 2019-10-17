d685d2d852bcd8d6de35819905ac39c76cabcf32
vishwanath7 <ezhilvishwanath@gmail.com>
2019-10-17 19:52:49 -0400

    Feature: Included Addition and Testing for addition

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,5 @@
 
-class Calculator:
-    result = 0
+def addition(a,b):
+    return a+b
 
-    def __init__(self):
-        pass
+
--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -1,12 +1,31 @@
 import unittest
-from Calculator import Calculator
+import Calculator
+
+class KnownValues(unittest.TestCase):
+    # Test addition ()
+    def test_addition(self):
+        results=Calculator.addition(1,2)
+        self.assertEqual(3, results)
 
 
-class MyTestCase(unittest.TestCase):
-
-    def test_instantiate_calculator(self):
-        calculator=Calculator()
-        self.assertIsInstance(calculator, Calculator)
-
-if __name__ == '__main__':
+if __name__== '__main__':
     unittest.main()
+
+
+
+
+
+
+
+
+
+import unittest
+#from Calculator import Calculator
+#class MyTestCase(unittest.TestCase):
+#
+#   def test_instantiate_calculator(self):
+#        calculator=Calculator()
+#        self.assertIsInstance(calculator, Calculator)
+
+#if __name__ == '__main__':
+#    unittest.main()
