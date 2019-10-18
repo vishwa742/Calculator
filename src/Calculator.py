6d6702ad2d6d3543ccacf436fd3c79613aec5cc1
vishwanath7 <ezhilvishwanath@gmail.com>
2019-10-17 21:19:14 -0400

    Feature: Set up calculator with add method

--- src/Calculator.py
+++ src/Calculator.py
@@ -6,9 +6,12 @@ class Calc:
     result=0
 
     def __init__(self):
-        x = 2+2
+        x = 0
         self.result = x;
         pass
 
+    def add(self, a, b):
+        c = a+b
+        return c
 #def subtraction(a,b):
     #return a-b
--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -2,43 +2,42 @@ import unittest
 import Calculator
 from Calculator import Calc
 
+calculator = Calc()
+
 class KnownValues(unittest.TestCase):
-    # Test addition ()
-    def test_addition(self):
-        results=Calculator.addition(1,2)
-        self.assertEqual(3, results)
 
     def test_instantiate_calculator(self):
-        calculator = Calc()
+        #calculator = Calc()
         self.assertIsInstance(calculator, Calc)
 
-    def test_results_property_calculator(self):
-        calculator = Calc()
-        self.assertEqual(calculator.result, 4)
+    def test_addition(self):
+        #calculator = Calc()
+        results=Calculator.addition(1,2)   # Calling the function from Calculator.py
+        self.assertEqual(3, results)
+        self.assertEqual(calculator.add(2,2), 4)  # Using the object add from the class Calc
+
+
 
 
     #def test_subtraction(self):
         #results=Calculator.subtraction(2,1)
         #self.assertEqual(1, results)
 
+    def test_results_property_calculator(self):
+        calculator = Calc()
+        self.assertEqual(calculator.result, 0)
+
+
 if __name__== '__main__':
     unittest.main()
 
 
 
+    #def test_add_method_calculator(self):
+        #calculator = Calc()
+        #self.assertEqual(calculator.add(2,2), 4)
 
 
 
 
 
-
-import unittest
-#from Calculator import Calculator
-#class MyTestCase(unittest.TestCase):
-#
-#   def test_instantiate_calculator(self):
-#        calculator=Calculator()
-#        self.assertIsInstance(calculator, Calculator)
-
-#if __name__ == '__main__':
-#    unittest.main()
