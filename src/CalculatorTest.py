1998ed81359379eb643f79ff31fe91b57eebe5db
vishwanath7 <ezhilvishwanath@gmail.com>
2019-10-17 22:38:46 -0400

    Fix: Updated test names

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -9,11 +9,11 @@ class KnownValues(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(calculator, Calc)
 
-    def test_addition(self):
+    def test_add_method(self):
         self.assertEqual(calculator.add(2,2), 4)
         self.assertEqual(calculator.result, 4)
 
-    def test_subtraction(self):
+    def test_subtract_method(self):
         self.assertEqual(calculator.subtract(2,2), 0)  # Using the object add from the class Calc
         self.assertEqual(calculator.result, 0)
 
